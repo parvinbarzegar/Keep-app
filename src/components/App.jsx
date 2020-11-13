cd3eba80cfd18b9585db3bef17022207f5d123f1
unknown <Parvin Barzegar>
2020-11-13 16:12:52 +0100

    keeper app part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-
+import notes from "../note"
 function App(){
     return <div>
 
     <Header/>
-    <Note/>
+      {notes.map(item =><Note title= {item.title } content = {item.content} key ={item.key}/>)}
     <Footer/>
     
     </div>;
